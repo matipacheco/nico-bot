42c5122db82d243f45c4aea4eb1bdc7f796ebd76
Mati <matipacheco@gmail.com>
2018-07-26 19:03:53 -0400

    ¨First commit. API works, it return random messages from file

---
+++ app.py
@@ -0,0 +1,25 @@
+import random
+
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+
+@app.route('/enlighten_me')
+def enlighten_me():
+	quotes = []
+	file   = open("misunderstood_wisdom.txt", "r")
+	
+	for quote in file:
+		quotes.append(quote.rstrip("\n"))
+
+	file.close()
+
+	return jsonify(quote = random.choice(quotes))
