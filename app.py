51faa11e9f6357e92fede968a423d16f5325bf63
Mati <matipacheco@gmail.com>
2018-07-26 19:05:30 -0400

    Adding the image and the view. It doesnt work... damn routes

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from flask import render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+
 @app.route('/enlighten_me')
 def enlighten_me():
 	quotes = []
