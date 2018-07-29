6f4edfc0222d00adbd00b09f8fe4d99089a44b00
Mati <matipacheco@gmail.com>
2018-07-29 10:36:00 -0400

    new view with JSON response

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import random
 import requests
 
 from flask import Flask
+from flask import jsonify
+
 
 app = Flask(__name__)
 
@@ -24,4 +26,18 @@ def enlighten_me():
 	slack_webhook_url = "https://hooks.slack.com/services/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"
 	requests.post(slack_webhook_url, data = payload, headers = { 'Content-type': 'application/json' })
 
-	return quote
+	return ''
+
+@app.route('/enlighten_me_json')
+def enlighten_me_json():
+	quotes = []
+	file   = open("misunderstood_wisdom.txt", "r")
+	
+	for quote in file:
+		quotes.append(quote.rstrip("\n"))
+
+	file.close()
+
+	quote   = random.choice(quotes)
+
+	return jsonify(text = quote)
