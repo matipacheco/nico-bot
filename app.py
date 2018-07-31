48bc2478322b9bb3e3fb1cee020f2b88f876e969
Mati <matipacheco@gmail.com>
2018-07-31 07:21:19 -0400

    print

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/enlighten_me', methods = ('GET', 'POST'))
 def enlighten_me():
-	print(request.args)
+	print(request.args.iterlists())
 	quotes = []
 	file   = open("misunderstood_wisdom.txt", "r")
 	
