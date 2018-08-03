e51f7f9af4a0f837cdaaa75e65e97b58d66b2557
Mati <matipacheco@gmail.com>
2018-08-03 19:03:04 -0400

    switch between options

--- app.py
+++ app.py
@@ -12,15 +12,25 @@ app = Flask(__name__)
 
 @app.route('/enlighten_me', methods = ('GET', 'POST'))
 def enlighten_me():
-	print(request.form.get("text"))
 	quotes = []
-	file   = open("misunderstood_wisdom.txt", "r")
+
+	text   = request.form.get("text").lower()
+	text   = unidecode.unidecode(text)
+	words  = text.split(" ")
 	
+	valid_words = ["podria", "puedo", "podemos", "podriamos"]
+
+	if (set(valid_words) & set(words)):
+		file = open("absolutely_no.txt", "r")
+
+	else:
+		file = open("misunderstood_wisdom.txt", "r")
+	
+
 	for quote in file:
 		quotes.append(quote.rstrip("\n"))
 
 	file.close()
+	quote = random.choice(quotes)
 
-	quote   = random.choice(quotes)
-	
 	return jsonify(text = quote, response_type = "in_channel")
