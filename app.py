e7945300fb7639efa9124d7aada5fc3fd3ed3518
Mati <matipacheco@gmail.com>
2018-08-03 18:43:49 -0400

    printing the params

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/enlighten_me', methods = ('GET', 'POST'))
 def enlighten_me():
-	print(request.args.get("text"))
+	print(request.get_json())
 	quotes = []
 	file   = open("misunderstood_wisdom.txt", "r")
 	
